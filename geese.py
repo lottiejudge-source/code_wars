31ddf0ff689d39f9abc0a95cb4436ef0d79b2f22
lottie Judge <lottie.judge@madetech.com>
2025-11-06 16:59:26 +0000

    no geese in list

--- geese.py
+++ geese.py
@@ -5,17 +5,28 @@ class Geese_Removal_Test(unittest.TestCase):
         self.assertIsInstance(geese, list)
     
     def test_removal_of_geese(self):
-        self.assertListEqual(non_geese, geese_removed)
+        birds = ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+        expected = ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+        result = geese_removal_function(birds)
+        self.assertListEqual(result, expected)
+    
+    def test_no_geese_in_list(self):
+        birds = ["Mallard", "Hook Bill", "Crested"]
+        expected = ["Mallard", "Hook Bill", "Crested"]
+        result = geese_removal_function(birds)
+        self.assertListEqual(result, expected)
+
     
     
 
 
 geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
 
-def geese_removal_function(birds):
-    geese_removed = ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
-    non_geese = ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
 
+def geese_removal_function(birdArray):
+    geese_removed = [bird for bird in birdArray if bird not in geese]
+
+    return geese_removed
 
 
 
